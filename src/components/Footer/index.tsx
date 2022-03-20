b47f9f07ddcf76bacdbb52029cec3e64fa6ea839
julianv97 <julian.vicente@radiumrocket.com>
2022-03-20 00:37:22 -0300

    JC: basic setup for header and footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Text, HStack } from '@chakra-ui/react';
+import { Text, HStack, Flex, Icon } from '@chakra-ui/react';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import { IoMdRocket } from 'react-icons/io';
 
 const Footer = () => {
   return (
-    <HStack width="full" backgroundColor="red.200">
-      <Text>Footer</Text>
+    <HStack width="100%" backgroundColor="red.200" justifyContent="space-between">
+      <Flex>
+        <Text>Codeado con React y ChakraUI</Text>
+      </Flex>
+      <Flex>
+        <Icon as={BsGithub} />
+        <Icon as={BsLinkedin} />
+        <Icon as={IoMdRocket} />
+      </Flex>
     </HStack>
   );
 };
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { HStack, Text } from '@chakra-ui/react';
+import { Flex, HStack, Text, Icon } from '@chakra-ui/react';
+import { MdDarkMode } from 'react-icons/md';
+import { BsFillSunFill } from 'react-icons/bs';
 
 const Header = () => {
   return (
-    <HStack width="100%" backgroundColor="red.200">
-      <Text>Header</Text>
+    <HStack width="100%" backgroundColor="red.200" justifyContent="space-between">
+      <Flex width="50%" justifyContent="space-around">
+        <Text>Home</Text>
+        <Text>Projects</Text>
+        <Text>Blog</Text>
+        <Text>About</Text>
+        <Text>Contact</Text>
+      </Flex>
+      <Flex>
+        <Icon as={MdDarkMode} />
+        <Icon as={BsFillSunFill} />
+      </Flex>
     </HStack>
   );
 };
