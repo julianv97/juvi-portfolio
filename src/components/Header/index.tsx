fd5507c6f15ec4da2969228ed52ef78bd1a4a39b
julianv97 <julian.vicente@radiumrocket.com>
2022-03-21 21:51:00 -0300

    JP: remove fixed header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ import ThemeButton from '../ThemeButton';
 
 const Header = () => {
   return (
-    <Stack position="fixed" z-zIndex={10} backgroundColor="white" top={0} width="60%">
+    <Stack width="60%">
       <HStack height="24" justifyContent="space-between">
         <Flex width="50%" justifyContent="space-around">
           <NavList />
