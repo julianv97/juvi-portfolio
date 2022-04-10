f478c36eeacb5cce9f50f42832b5e1895e879058
julianv97 <julian.vicente@radiumrocket.com>
2022-04-10 17:26:42 -0300

    JP: refactor switch theme button

--- src/components/ThemeButton/index.tsx
+++ src/components/ThemeButton/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { useColorMode } from '@chakra-ui/react';
+import { useColorMode, useColorModeValue } from '@chakra-ui/react';
 import { BsFillSunFill } from 'react-icons/bs';
 import { MdDarkMode } from 'react-icons/md';
 import TooltipIcon from '../TooltipIcon';
 
 const ThemeButton = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const Icon = useColorModeValue(MdDarkMode, BsFillSunFill);
+
   return (
     <TooltipIcon
       label={colorMode === 'light' ? 'Dark mode' : 'Light mode'}
-      icon={colorMode === 'light' ? MdDarkMode : BsFillSunFill}
+      icon={Icon}
       onClick={toggleColorMode}
     />
   );
