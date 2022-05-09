208226e28cbd33dea77a5e33565a846d92c8f1cc
julianv97 <julian.vicente@radiumrocket.com>
2022-05-08 22:59:39 -0300

    JP: basic layout for post page

--- src/screens/Blog/SinglePostPage/index.tsx
+++ src/screens/Blog/SinglePostPage/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Flex, HStack, Stack, Heading, Image } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
+
+import POSTS from '../../../data/postsList';
 
 const SinglePostPage: React.FC = () => {
   const { id } = useParams();
-  return <div>{id}</div>;
+  const post = POSTS.find((postItem) => postItem.id === Number(id));
+  return (
+    <Flex flexDirection="column" alignItems="center" w="full">
+      <HStack marginTop={20} w="80%">
+        <Image src={post?.image} />
+        <Stack w="100%" justifyContent="flex-start">
+          <Heading fontSize={30}>{post?.title}</Heading>
+        </Stack>
+      </HStack>
+    </Flex>
+  );
 };
 
 export default SinglePostPage;
