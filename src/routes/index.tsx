c179c20116c7aab3cc4645f859b1d1880f5a4ab4
julianv97 <julian.vicente@radiumrocket.com>
2022-05-08 22:21:57 -0300

    JP: add lazy import for routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { Spinner } from '@chakra-ui/react';
 
-import HomeRoutes from './home';
-import ProjectsRoutes from './projects';
-import ContactRoutes from './contact';
-import BlogRoutes from './blog';
-import AboutRoutes from './about';
+const HomeRoutes = lazy(() => import('./home'));
+const ProjectsRoutes = lazy(() => import('./projects'));
+const ContactRoutes = lazy(() => import('./contact'));
+const BlogRoutes = lazy(() => import('./blog'));
+const AboutRoutes = lazy(() => import('./about'));
 
 const Router = () => {
   return (
-    <Routes>
-      <Route path="/home" element={<HomeRoutes />} />
-      <Route path="/projects" element={<ProjectsRoutes />} />
-      <Route path="/contact" element={<ContactRoutes />} />
-      <Route path="/blog/*" element={<BlogRoutes />} />
-      <Route path="/about" element={<AboutRoutes />} />
-      <Route path="*" element={<Navigate to="/home" replace />} />
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/home" element={<HomeRoutes />} />
+        <Route path="/projects/*" element={<ProjectsRoutes />} />
+        <Route path="/contact" element={<ContactRoutes />} />
+        <Route path="/blog/*" element={<BlogRoutes />} />
+        <Route path="/about" element={<AboutRoutes />} />
+        <Route path="*" element={<Navigate to="/home" replace />} />
+      </Routes>
+    </Suspense>
   );
 };
 
