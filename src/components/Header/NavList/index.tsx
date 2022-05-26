3682166d5a82ee9248db6c2f1f0b65614acaaa11
julianv97 <julian.vicente@radiumrocket.com>
2022-05-25 23:08:48 -0300

    JP: add hamburguer menu with drawer

--- src/components/Header/NavList/index.tsx
+++ src/components/Header/NavList/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Flex } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
 import NAV_LINKS from '../constants';
 
 const NavList = () => {
   return (
-    <>
+    <Flex w="100%" flexDirection={['column', 'column', 'row']} justifyContent="space-between">
       {NAV_LINKS.map((link) => (
         <RouterLink key={link.name} to={link.path}>
           {link.name}
         </RouterLink>
       ))}
-    </>
+    </Flex>
   );
 };
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,67 @@
-import React from 'react';
-import { Flex, HStack, Divider, Stack } from '@chakra-ui/react';
+import React, { useRef } from 'react';
+import {
+  Flex,
+  HStack,
+  Divider,
+  Stack,
+  useDisclosure,
+  Box,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  DrawerFooter,
+  Button,
+} from '@chakra-ui/react';
+import { HiMenuAlt4 } from 'react-icons/hi';
 import NavList from './NavList';
 import ThemeButton from '../ThemeButton';
 
 const Header = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
+
   return (
-    <Stack width="60%">
+    <Stack width={['100%', '80%', '80%', '80%']}>
       <HStack height="24" justifyContent="space-between">
-        <Flex width="50%" justifyContent="space-around">
+        <Box display={['block', 'block', 'none']}>
+          <HiMenuAlt4
+            size="24"
+            onClick={isOpen ? onClose : onOpen}
+            cursor="pointer"
+            color="gray.500"
+          />
+        </Box>
+        <Box width="50%" display={['none', 'none', 'flex']}>
           <NavList />
-        </Flex>
+        </Box>
         <Flex>
           <ThemeButton />
         </Flex>
       </HStack>
       <Divider />
+      {/* @ts-ignore */}
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose} finalFocusRef={btnRef}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerCloseButton margin={2} ml={5} position="relative" left={0} />
+          </DrawerHeader>
+
+          <DrawerBody>
+            <NavList />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </Stack>
   );
 };
