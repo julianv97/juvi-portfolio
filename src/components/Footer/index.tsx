2c06bf866e792ea2eb4b80bce75ee0886d515837
julianv97 <julian.vicente@radiumrocket.com>
2022-05-25 21:58:28 -0300

    JP: add responsive styles for footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Text, HStack, Flex } from '@chakra-ui/react';
-import { MdCoffee } from 'react-icons/md';
 import SocialLinks from './SocialLinks';
 
 const Footer = () => {
   return (
-    <HStack mt={20} justifyContent="space-between" height="14" width="60%">
-      <Flex>
-        <Text>Made with React, ChakraUI and a lot of </Text>
-        <MdCoffee size="1.5em" />
-      </Flex>
+    <HStack
+      mt={20}
+      justifyContent="space-between"
+      height="14"
+      width={['100%', '80%', '80%', '80%']}
+    >
+      <Text fontSize="sm" color="gray.500">
+        Made with React, ChakraUI and a lot of
+        <span role="img" aria-label="love">
+          {' '}
+          ☕️
+        </span>{' '}
+        by Julián Vicente
+      </Text>
+
       <Flex>
         <SocialLinks />
       </Flex>
