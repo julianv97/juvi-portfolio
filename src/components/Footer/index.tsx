83088fafa4c5fdf63a5645fc4a679a5715672790
julianv97 <julian.vicente@radiumrocket.com>
2022-05-25 22:02:17 -0300

    JP: remove braces

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,28 +2,21 @@ import React from 'react';
 import { Text, HStack, Flex } from '@chakra-ui/react';
 import SocialLinks from './SocialLinks';
 
-const Footer = () => {
-  return (
-    <HStack
-      mt={20}
-      justifyContent="space-between"
-      height="14"
-      width={['100%', '80%', '80%', '80%']}
-    >
-      <Text fontSize="sm" color="gray.500">
-        Made with React, ChakraUI and a lot of
-        <span role="img" aria-label="love">
-          {' '}
-          ☕️
-        </span>{' '}
-        by Julián Vicente
-      </Text>
+const Footer = () => (
+  <HStack mt={20} justifyContent="space-between" height="14" width={['100%', '80%', '80%', '80%']}>
+    <Text fontSize="sm" color="gray.500">
+      Made with React, ChakraUI and a lot of
+      <span role="img" aria-label="love">
+        {' '}
+        ☕️
+      </span>{' '}
+      by Julián Vicente
+    </Text>
 
-      <Flex>
-        <SocialLinks />
-      </Flex>
-    </HStack>
-  );
-};
+    <Flex>
+      <SocialLinks />
+    </Flex>
+  </HStack>
+);
 
 export default Footer;
