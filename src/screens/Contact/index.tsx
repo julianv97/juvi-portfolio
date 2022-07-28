5c7a1254a2b98c30c3c28c5c566205fd93c5234d
julianv97 <julian.vicente@radiumrocket.com>
2022-07-27 22:33:37 -0300

    JP: contact screen done

--- src/screens/Contact/index.tsx
+++ src/screens/Contact/index.tsx
@@ -1,14 +1,32 @@
-import { Heading } from '@chakra-ui/react';
 import React from 'react';
+import { Button, Flex, Heading, Text, Box } from '@chakra-ui/react';
+import { AiFillLinkedin } from 'react-icons/ai';
+import { MdEmail } from 'react-icons/md';
 import ContactForm from '../../components/ContactForm';
 import Section from '../../components/Section';
 
 const Contacts = () => {
   return (
     <Section>
-      <Heading size="md" mb={10}>
-        You can contant me by email or the social networks what are in the footer!
+      <Heading size="xl" mb={10}>
+        Contact
       </Heading>
+      <Flex direction="column" alignItems="start">
+        <Button leftIcon={<MdEmail size="30" />} bg="inherit" mb={6}>
+          <a href="mailto:julian.vicente97s@gmail.com">Here is my email!</a>
+        </Button>
+        <Button leftIcon={<AiFillLinkedin size="30" />} bg="inherit" mb={6}>
+          <a href="https://www.linkedin.com/in/julian-vicente/" target="_blank" rel="noreferrer">
+            You can contact me in Linkedin!
+          </a>
+        </Button>
+      </Flex>
+      <Box pl={5} pb={7}>
+        <Text fontWeight="semibold">
+          Or you can send me an message using the form below. I will get back to you as soon as
+          possible.
+        </Text>
+      </Box>
       <ContactForm />
     </Section>
   );
